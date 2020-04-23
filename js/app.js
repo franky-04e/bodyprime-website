049f4df661452edb487cf038a85371974f39e929
Giancarlo Urone <giancarlo.urone@community.unipa.it>
2020-04-23 11:11:11 +0200

    Aggiunta animazione menu laterale burger

--- js/app.js
+++ js/app.js
@@ -15,6 +15,9 @@ const navSlide = () =>{
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
             }
         });
+
+        //Burger Animation
+        burger.classList.toggle('toogle');
     });
 }
 
